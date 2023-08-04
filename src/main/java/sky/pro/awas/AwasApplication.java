9b60fc981d3d18e26878c081e36caec75b22b2a3
Ann <niyta@mail.ru>
2023-08-04 22:06:52 +0500

    .

--- src/main/java/sky/pro/awas/AwasApplication.java
+++ src/main/java/sky/pro/awas/AwasApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AwasApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AwasApplication.class, args);
     }
-
 }
